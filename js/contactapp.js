de29d1fbe63e8329ede08d72e4e0af0ae3d2ca5e
kevin v.d windt <kevinv.dwindt@MacBook-van-kevin.local>
2018-09-14 13:07:42 +0200

    person info page working

--- js/contactapp.js
+++ js/contactapp.js
@@ -1,5 +1,5 @@
 let people;
-fetch('https://randomuser.me/api/?results=5&format=json').then(response => {
+fetch('https://randomuser.me/api').then(response => {
         return response.json();
     }).then(peopleResponse => {
     people = peopleResponse;
@@ -9,8 +9,13 @@ fetch('https://randomuser.me/api/?results=5&format=json').then(response => {
     });
 
 function peopleLength(){
-    let persons;
-
+    console.log(people);
+    document.getElementById("first__name").innerText = `${people.results[0].name.first} ${people.results[0].name.last}`;
+    document.getElementById("email").innerText = people.results[0].email;
+    document.getElementById("work__email").innerText = people.results[0].email;
+    document.getElementById("number").innerText = people.results[0].cell;
+    document.getElementById("work__number").innerText = people.results[0].phone;
+    document.getElementById("picture").src = people.results[0].picture.thumbnail;
 }
 
 
